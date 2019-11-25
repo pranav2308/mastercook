98f2e0394260bea9bd60185ab4ba4bd4e6657b56
Wang Eunice <eunicewang1995@gmail.com>
2019-11-25 01:21:07 -0500

    register functionality fixed

--- src/Components/Homepage/DivHelper.js
+++ src/Components/Homepage/DivHelper.js
@@ -6,7 +6,6 @@ const RegularDiv = (props)=> {
     <div class = "login-container">
       <Form>
 						<Form.Group>
-						<Form.Label>Email: </Form.Label>
 						<Form.Control id="email" type="email" placeholder="Enter Email" onChange={props.emailOnChange}/>
 						<Form.Text className="text-muted">
 							We'll never share your email with anyone else.
@@ -14,7 +13,6 @@ const RegularDiv = (props)=> {
 						</Form.Group>
 					
 						<Form.Group>
-						<Form.Label>Password: </Form.Label>
 						<Form.Control id="password" type="password" placeholder="Enter Password" onChange={props.passwordOnChange}/>
 						</Form.Group>
 						<Form.Group controlId="formCheckbox">
@@ -37,36 +35,30 @@ const RegisterDiv = (props)=> {
         <div class = "register-container">
         <Form>
             <Form.Group>
-            <Form.Label>First Name: </Form.Label>
-            <Form.Control id="first-name" type="text" placeholder="Enter First Name" />
+            <Form.Control id="first-name" type="text" placeholder="First Name" onChange={props.userFirstNameOnChange}/>
             </Form.Group>
 
             <Form.Group>
-            <Form.Label>Last Name: </Form.Label>
-            <Form.Control id="last-name" type="text" placeholder="Enter Last Name" />
+            <Form.Control id="last-name" type="text" placeholder="Last Name" onChange={props.userLastNameOnChange}/>
             </Form.Group>
 
             <Form.Group>
-            <Form.Label>Email: </Form.Label>
-            <Form.Control id="email" type="email" placeholder="Enter Email" />
+            <Form.Control id="email" type="email" placeholder="Email" onChange={props.userEmailOnChange}/>
             <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
             </Form.Text>
             </Form.Group>
 
             <Form.Group>
-            <Form.Label>Username: </Form.Label>
-            <Form.Control id="username" type="text" placeholder="Enter Username" />
+            <Form.Control id="username" type="text" placeholder="Username" onChange={props.usernameOnChange}/>
             </Form.Group>
         
             <Form.Group>
-            <Form.Label>Password: </Form.Label>
-            <Form.Control id="password" type="password" placeholder="Enter Password" />
+            <Form.Control id="password" type="password" placeholder="Password" onChange={props.userPasswordOnChange}/>
             </Form.Group>
 
             <Form.Group>
-            <Form.Label>Password: </Form.Label>
-            <Form.Control id="password" type="password" placeholder="Confirm Password" />
+            <Form.Control id="password" type="password" placeholder="Confirm Password" onChange={props.confirmedPasswordOnChange}/>
             </Form.Group>
 
             <Button variant="primary" type="button" onClick={props.handleRegister}>
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -12,17 +12,28 @@ class Homepage extends Component{
 
 		this.state = {
 			signedIn: false, 
-			userEmail: '' , 
-			userPassword: '', 
+			register: false,
 			error: false, 
 			message: '', 
-			register: false};
+			userEmail: '' , 
+			userPassword: '', 
+			userFirstName: '',
+			userLastName: '',
+			username: '',
+			userConfirmedPassword: '',
+		};
 
 		this.onLogin = this.onLogin.bind(this);
 		this.onSignup = this.onSignup.bind(this);
 		this.handleRegister = this.handleRegister.bind(this);
 		this.emailOnChange = this.emailOnChange.bind(this);
 		this.passwordOnChange = this.passwordOnChange.bind(this);
+		this.userFirstNameOnChange = this.userFirstNameOnChange.bind(this);
+		this.userLastNameOnChange = this.userLastNameOnChange.bind(this);
+		this.userEmailOnChange = this.userEmailOnChange.bind(this);
+		this.usernameOnChange = this.usernameOnChange.bind(this);
+		this.userPasswordOnChange = this.userPasswordOnChange.bind(this);
+		this.confirmedPasswordOnChange = this.confirmedPasswordOnChange.bind(this);
 	}
 
 	emailOnChange(event) {
@@ -73,11 +84,65 @@ class Homepage extends Component{
 		
 	}
 
+	userFirstNameOnChange(event) {
+		this.setState({userFirstName: event.target.value});
+	}
+
+	userLastNameOnChange(event) {
+		this.setState({userLastName: event.target.value});
+	}
+
+	userEmailOnChange(event) {
+		this.setState({userEmail: event.target.value});
+	}
+
+	usernameOnChange(event) {
+		this.setState({username: event.target.value});
+	}
+
+	userPasswordOnChange(event) {
+		this.setState({userPassword: event.target.value});
+	}
+
+	confirmedPasswordOnChange(event) {
+		this.setState({userConfirmedPassword: event.target.value});
+	}
+
 	handleRegister(event){
 
-		firebase.auth.createUserWithEmailAndPassword(this.userEmail, this.userPassword)
+		if (this.state.userFirstName == ''){
+			alert('First Name Cannot Be Blank.');
+		}
+		if (this.state.userLastName == ''){
+			alert('Last Name Cannot Be Blank.');
+		}
+		if (this.state.userEmail == ''){
+			alert('Email Cannot Be Blank.');
+		}
+		if (this.state.username == ''){
+			alert('Username Cannot Be Blank.');
+		}
+		if (this.state.userPassword == ''){
+			alert('Password Cannot Be Blank.');
+		}
+		if (this.state.userPassword.length < 8){
+			alert('Password Must Be More Than 8 Characters.')
+		}
+		if (this.state.userConfirmedPassword == ''){
+			alert('Please Confirm Password.');
+		}
+		if (this.state.userPassword != this.state.userConfirmedPassword) {
+			alert('Password and Confirmation Do Not Match.');
+		}
+
+		firebase.auth.createUserWithEmailAndPassword(this.state.userEmail, this.state.userPassword)
 			.then(() => {
 				this.setState({signedIn: true, error: false, message: '', register: false});
+				firebase.firestore.collection("Users").doc(this.state.username).set({
+					FirstName: this.state.userFirstName,
+					LastName: this.state.userLastName,
+					Email: this.state.userEmail,
+				}).then(() => { console.log("account created");})
 			})
 			.catch((error) => {
 				let errorCode = error.code;
@@ -106,13 +171,24 @@ class Homepage extends Component{
 		if (this.state.register) {
 			return (
 				<div class = "homepage-container">
-					<RegisterDiv handleRegister = {this.handleRegister}/>}
+					<div class="bg-image">
+					<RegisterDiv 
+						handleRegister = {this.handleRegister}	
+						userFirstNameOnChange = {this.userFirstNameOnChange}
+						userLastNameOnChange = {this.userLastNameOnChange}
+						userEmailOnChange = {this.userEmailOnChange}
+						usernameOnChange = {this.usernameOnChange}
+						userPasswordOnChange = {this.userPasswordOnChange}
+						confirmedPasswordOnChange = {this.confirmedPasswordOnChange}
+					/>
+					</div>
 				</div>
 			);
 		}
 
 		return(
 			<div class = "homepage-container">
+				<div><font size="7" color="white">Join Us Now!</font></div>
 				<RegularDiv onSignup = {this.onSignup} onLogin = {this.onLogin} emailOnChange = {this.emailOnChange} passwordOnChange = {this.passwordOnChange}/>
 			</div>
 			
