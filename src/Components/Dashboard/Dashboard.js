647fcb0a37360980537d77477a4b6c8dd9c54567
pranav2308 <pranav08@terpmail.umd.edu>
2019-11-28 14:42:56 -0500

    Added responsive grids

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -27,28 +27,83 @@ const Dashboard = (props) => {
 				{announcementList}
 				{assignmentList}
 			</div>
-			
-			<h2 className = 'heading'> Enrolled courses</h2>
-			<div className = "card dashboard-addon">
-			  <img src= {tofuImgUrl} class="card-img-top" alt="Tofu dishes"/>
-			  <div class="card-body">
-			    <h5 class="card-title">Tofu recipes</h5>
-			    <p class="card-text">Learn delicious tofu recipes at low cost. It's fast, healthy, and inexpensive.</p>
-			    <a class="btn btn-primary">Continue course</a>
-			  </div>
-			  <div class="progress">
-			  	<div className="progress-bar bg-success progress-bar-addon" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
-			  </div>
-			</div>
+			<div className = "main-container container">
+				<div className = "row">
+					<div className = "col">
+						<h2 className = 'heading text-left'> Enrolled courses</h2>
+					</div>
+				</div>
+				<div className = "row">
+					
+					<div className = "col">
+						<div className = "card card-addon">
+						  <img src= {tofuImgUrl} class="card-img-top" alt="Tofu dishes"/>
+						  <div class="card-body">
+						    <h5 class="card-title">Tofu recipes</h5>
+						    <p class="card-text">Learn delicious tofu recipes at low cost. It's fast, healthy, and inexpensive.</p>
+						    <p class="card-text"><span className = "font-weight-bold">Intructor:</span> Vladimir Putin</p>
+						    <a class="btn btn-primary">Continue course</a>
+						  </div>
+						  <div class="progress">
+						  	<div className="progress-bar bg-success progress-bar-addon" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+						  </div>
+						</div>
+					</div>
 
-			<h2 className = 'heading'> Recommended courses</h2>
-			<div className = "card dashboard-addon">
-			  <img src= {chickenImgUrl} class="card-img-top" alt="Tofu dishes"/>
-			  <div class="card-body">
-			    <h5 class="card-title">Chicken recipes</h5>
-			    <p class="card-text">Cut chicken and eat it!</p>
-			    <a class="btn btn-primary">Learn more!</a>
-			  </div>
+					<div className = "col">
+						<div className = "card card-addon">
+						  <img src= {tofuImgUrl} class="card-img-top" alt="Tofu dishes"/>
+						  <div class="card-body">
+						    <h5 class="card-title">Tofu recipes</h5>
+						    <p class="card-text">Learn delicious tofu recipes at low cost. It's fast, healthy, and inexpensive.</p>
+						     <p class="card-text"><span className = "font-weight-bold">Intructor:</span> Vladimir Putin</p>
+						    <a class="btn btn-primary">Continue course</a>
+						  </div>
+						  <div class="progress">
+						  	<div className="progress-bar bg-success progress-bar-addon" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+						  </div>
+						</div>
+					</div>
+
+					<div className = "col">
+						<div className = "card card-addon">
+						  <img src= {tofuImgUrl} class="card-img-top" alt="Tofu dishes"/>
+						  <div class="card-body">
+						    <h5 class="card-title">Tofu recipes</h5>
+						    <p class="card-text">Learn delicious tofu recipes at low cost. It's fast, healthy, and inexpensive.</p>
+						     <p class="card-text"><span className = "font-weight-bold">Intructor:</span> Vladimir Putin</p>
+						    <a class="btn btn-primary">Continue course</a>
+						  </div>
+						  <div class="progress">
+						  	<div className="progress-bar bg-success progress-bar-addon" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+						  </div>
+						</div>
+					</div>
+
+				</div>
+				
+				<div className = "row">
+					<div className = "col">
+						<h2 className = 'heading text-left'> Recommended courses </h2>
+					</div>
+				</div>
+
+				<div className = "row">
+
+					<div className = "col">
+						<div className = "card card-addon">
+						  <img src= {chickenImgUrl} class="card-img-top" alt="Tofu dishes"/>
+						  <div class="card-body">
+						    <h5 class="card-title">Chicken recipes</h5>
+						    <p class="card-text">Cut chicken and eat it!</p>
+						     <p class="card-text"><span className = "font-weight-bold">Intructor:</span> Kim jong-un</p>
+						    <a class="btn btn-primary">Enroll Course!</a>
+						  </div>
+						</div>
+					</div>
+
+				</div>
+				
 			</div>
 		</div>
 	);
