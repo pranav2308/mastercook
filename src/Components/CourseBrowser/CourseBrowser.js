0d964474ab35c794facf4413f64b36c3f3bc6d50
Wang Eunice <eunicewang1995@gmail.com>
2019-11-30 01:42:21 -0500

    added course item

--- src/Components/CourseBrowser/CourseBrowser.js
+++ src/Components/CourseBrowser/CourseBrowser.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import {Dropdown} from 'react-bootstrap';
 import firebase from '../../Firebase/firebase';
 import './CourseBrowser.css';
+import CourseItem from './CourseItem';
 
 const CourseBrowser = () => {
 
@@ -15,67 +17,42 @@ const CourseBrowser = () => {
 		fontWeight: 'bold',
 		fontSize: '350%'
 	}
-	const tofuImgUrl = "https://www.usnews.com/dims4/USNEWS/4f17cab/2147483647/thumbnail/640x420/quality/85/?url=http%3A%2F%2Fcom-usnews-beam-media.s3.amazonaws.com%2Fa9%2F14%2Fc75d491f4979a756e6108846c2e5%2F161104-tofucubes-stock.jpg"
+	
+	
 	return (
 		<div>
 			<div class="optionContainer">
-				<div class="dropdown show">
-					<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-						Sort by
-					</a>
+				<div class="sort-by-container">
+				<Dropdown>
+					<Dropdown.Toggle variant="light" id="dropdown-basic">
+						Sort By
+					</Dropdown.Toggle>
 
-					<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-						<a class="dropdown-item" href="#">Most relevant</a>
-						<a class="dropdown-item" href="#">Recent</a>
-						<a class="dropdown-item" href="#">Name (A->Z)</a>
-						<a class="dropdown-item" href="#">Name (Z->A)</a>
-					</div>
+					<Dropdown.Menu>
+						<Dropdown.Item href="#/action-1">Most relevant</Dropdown.Item>
+						<Dropdown.Item href="#/action-2">Most recent</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Name (A->Z)</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Name (Z->A)</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 				</div>
-				<div class="dropdown show">
-					<a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<div class="categories-container">
+				<Dropdown>
+					<Dropdown.Toggle variant="light" id="dropdown-basic">
 						Categories
-					</a>
+					</Dropdown.Toggle>
 
-					<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-						<a class="dropdown-item" href="#">All</a>
-						<a class="dropdown-item" href="#">Indian</a>
-						<a class="dropdown-item" href="#">Thai</a>
-						<a class="dropdown-item" href="#">Chinese</a>
-						
-					</div>
-					
+					<Dropdown.Menu>
+						<Dropdown.Item href="#/action-1">All</Dropdown.Item>
+						<Dropdown.Item href="#/action-2">Indian</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Thai</Dropdown.Item>
+						<Dropdown.Item href="#/action-3">Chinese</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
 				</div>
 			</div>
-
-			<section class="resultCard">
-				<div class="card">
-					<div class="row ">
-						<div class="col-md-3">
-							<img src={tofuImgUrl} class="resultCardImage"/>
-						</div>
-						<div class="col-md-8 px-3">
-							<div class="card-block px-3">
-							<p class="card-text">Name: Cooking with Babish</p>
-							<p class="card-text">Description: Blah Blah</p>
-							<p class="card-text"> Duration: 1 hour</p>
-							<p class="card-text"> Rating: 4.5/5</p>
-							<button class="btn btn-primary" type="submit">Enroll Now</button>
-							</div>
-						</div>
-
-						</div>
-					</div>
-
-
-			</section>
 			
-			<table style={tableStyle}>
-				<tbody>
-					<tr>
-						<td className="align-middle text-center" style={bigHeadingStyle}>Course Browser Page</td>
-					</tr>
-				</tbody>
-			</table>
+			<div> <CourseItem /></div>
 		</div>
 	);
 }
