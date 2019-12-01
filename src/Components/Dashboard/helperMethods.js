808995ade537a3f11e6a7960936d338515af7b7f
pranav2308 <pranav08@terpmail.umd.edu>
2019-12-01 12:50:30 -0500

    merge commit

--- src/Components/Dashboard/helperMethods.js
+++ src/Components/Dashboard/helperMethods.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import firebase from '../../Firebase/firebase';
+import React from "react";
+import firebase from "../../Firebase/firebase";
 
-
-async function setEnrolledCourseList(enrolledCourses){
-
-	const promises = enrolledCourses.map(enrolledCourseObj => {
-		return new Promise((resolve, reject)=>{
-			firebase.firestore.collection("Courses").doc(enrolledCourseObj.courseID.toString()).onSnapshot(snapshot => 
-																											{let databaseObj = snapshot.data()
-																												databaseObj.progress = enrolledCourseObj.progress
-																												resolve(databaseObj)});	
-		})
-	});
-	const enrolledCourseList = await Promise.all(promises);
-	this.setState({enrolledCourseList : enrolledCourseList});
+async function setEnrolledCourseList(enrolledCourses) {
+  const promises = enrolledCourses.map(enrolledCourseObj => {
+    return new Promise((resolve, reject) => {
+      firebase.firestore
+        .collection("Courses")
+        .doc(enrolledCourseObj.courseID.toString())
+        .onSnapshot(snapshot => {
+          let databaseObj = snapshot.data();
+          databaseObj.progress = enrolledCourseObj.progress;
+          resolve(databaseObj);
+        });
+    });
+  });
+  const enrolledCourseList = await Promise.all(promises);
+  this.setState({ enrolledCourseList: enrolledCourseList });
 }
 
 
@@ -43,66 +45,75 @@ function getEnrolledCoursesRenderElement(enrolledCourseList){
 	}
 
 	return (
-		<div className = "col">
-			<p className = 'font-weight-light font-italic text-left'> You currently have no Enrolled Courses!</p>
-		</div>
-	);
+    <div className="col">
+      <p className="font-weight-light font-italic text-left">
+        {" "}
+        You currently have no Enrolled Courses!
+      </p>
+    </div>
+  );
 }
 
+function getAssignmentRenderElement(enrolledCourseList) {
+  const assignmentRenderElement = enrolledCourseList.map(enrolledCourseObj => {
+    const { Assignments } = enrolledCourseObj;
+    if (Assignments) {
+      const assignmentList = Assignments.map(assignmentID => (
+        <li> {`Assignment ${assignmentID}`}</li>
+      ));
+      return (
+        <div>
+          <h4> {enrolledCourseObj.Name} </h4>
+          {assignmentList}
+        </div>
+      );
+    }
+  });
 
-function getAssignmentRenderElement(enrolledCourseList){
-	const assignmentRenderElement = enrolledCourseList.map((enrolledCourseObj) => {
-		const { Assignments } = enrolledCourseObj;
-		if(Assignments){
-			const assignmentList = Assignments.map((assignmentID) => <li> {`Assignment ${assignmentID}`}</li>)
-			return(
-				<div>
-					<h4> {enrolledCourseObj.Name} </h4>
-					{assignmentList}
-				</div>
-			);
-		}
-	});
-
-	if(assignmentRenderElement.length !== 0){
-		return (
-			<div className = "sidebar-list">
-				<h3> Assignment </h3>
-					{assignmentRenderElement}
-			</div>
-		);	
-	}
-	
+  if (assignmentRenderElement.length !== 0) {
+    return (
+      <div className="sidebar-list">
+        <h3> Assignment </h3>
+        {assignmentRenderElement}
+      </div>
+    );
+  }
 }
 
-async function setAnnouncementObj(){
-
-	const announcementObj = await new Promise((resolve, reject) => {
-			firebase.firestore.collection("Announcements").doc("AnnouncementList").onSnapshot(snapshot => {
-			resolve(snapshot.data());
-		})
-	});
-	this.setState({announcementObj : announcementObj});
-	// console.log(announcementObj);
+async function setAnnouncementObj() {
+  const announcementObj = await new Promise((resolve, reject) => {
+    firebase.firestore
+      .collection("Announcements")
+      .doc("AnnouncementList")
+      .onSnapshot(snapshot => {
+        resolve(snapshot.data());
+      });
+  });
+  this.setState({ announcementObj: announcementObj });
+  // console.log(announcementObj);
 }
 
-function getAnnouncementRenderElement(announcementObj){
-	if(announcementObj){
-
-		let announcementList = [];
-		for (const announcementID in announcementObj){
-			announcementList.push(<li> {announcementObj[announcementID]}</li>)
-		}
-		if(announcementList){
-			return(
-				<div className = "sidebar-list">
-					<h3> Announcement </h3>
-					{announcementList}
-				</div>
-			);
-		}	
-	}
-	 
+function getAnnouncementRenderElement(announcementObj) {
+  if (announcementObj) {
+    let announcementList = [];
+    for (const announcementID in announcementObj) {
+      announcementList.push(<li> {announcementObj[announcementID]}</li>);
+    }
+    if (announcementList) {
+      return (
+        <div className="sidebar-list">
+          <h3> Announcement </h3>
+          {announcementList}
+        </div>
+      );
+    }
+  }
 }
 
-export { setEnrolledCourseList, getEnrolledCoursesRenderElement, getAssignmentRenderElement, setAnnouncementObj, getAnnouncementRenderElement };
+export {
+  setEnrolledCourseList,
+  getEnrolledCoursesRenderElement,
+  getAssignmentRenderElement,
+  setAnnouncementObj,
+  getAnnouncementRenderElement
+};
