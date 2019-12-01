fea8fe86821790c63c1b3d7f9f9b3bfc1077fd29
KhushbooKhathuria <55103711+KhushbooKhathuria@users.noreply.github.com>
2019-12-01 01:20:15 -0500

    Update QuestionList.js

--- src/Components/Assignment/QuestionList.js
+++ src/Components/Assignment/QuestionList.js
@@ -6,43 +6,43 @@ const QuestionList = (props) => {
      <div class = "questionsContainer">
         <form>
           <div class="form-group question-answer">
-            <label id = "question-1" className = "question-text">Question 1. What do you add sugar or salt while making rice?   (2 pts)</label>
+            <label id = "question-1" className = "question-text">Question 1. Can you use coconut oil instead of butter while making a cookiee(Yes/No)?   (2 pts)</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" onChange = {(event) => props.onChangeTextAnswers(event, 1)}/>
           </div>
           
           <div class="form-group  question-answer">
-            <label id = "question-2" className = "question-text">Question 2. What is the colour of turmeric?  (2 pts)</label>
+            <label id = "question-2" className = "question-text">Question 2. Which ingredient would you use before you bake?  (2 pts)</label>
             <select class="form-control" id="exampleFormControlSelect1" onChange = {(event) => props.onChangeSelectAnswers(event, 2)}>
               <option>Select Answer</option>
-              <option>Yellow</option>
-              <option>Red</option>
-              <option>Brown</option>  
+              <option>Cooking spray</option>
+              <option>Flour</option>
+              <option>Butter</option>  
             </select>
           </div>
 
           <div class="form-group question-answer">
-            <label id = "question-3" className = "question-text">Question 3. Which of the following elements cannot be added in pasta?  (2 pts)</label>
+            <label id = "question-3" className = "question-text">Question 3. Which of the following is a cooking aid?  (2 pts)</label>
             <select class="form-control" id="exampleFormControlSelect2" onChange = {(event) => props.onChangeSelectAnswers(event, 3)}>
               <option>Select Answer</option>
-              <option>Salt</option>
-              <option>Pepper</option>
-              <option>Flour</option>
-              <option>Zucchini</option>
+              <option>Parchment Paper</option>
+              <option>Wax paper</option>
+              <option>Aluminium foil</option>
+              <option>Cooling paper</option>
             </select>
           </div>
 
           <div class="form-group  question-answer">
-            <label id = "question-4" className = "question-text">Question 4. What is the color of chilli powder?  (2 pts)</label>
+            <label id = "question-4" className = "question-text">Question 4. Name an ingredient used in making a chocolate chip cookie dough.  (2 pts)</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" onChange = {(event) => props.onChangeTextAnswers(event, 4)}></textarea>
           </div>
 
           <div class="form-group  question-answer">
-            <label  id = "question-5" className = "question-text">Question 5. What do julienne cuts look like?  (2 pts)</label>
+            <label  id = "question-5" className = "question-text">Question 5. Name a flour that can be used in making cookie dough?  (2 pts)</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" onChange = {(event) => props.onChangeTextAnswers(event, 5)}/>
           </div>
 
           <div class="form-group question-answer">
-            <label id = "question-6" className = "question-text">Question 6. When you cook your pasta al dente, what should the texture be?  (2 pts)</label>
+            <label id = "question-6" className = "question-text">Question 6. Is it necessary to use unsalted butter while baking a cookie? (2 pts)</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" onChange = {(event) => props.onChangeTextAnswers(event, 6)}/>
           </div>
 
@@ -57,26 +57,26 @@ const QuestionList = (props) => {
           </div>
 
           <div class="form-group question-answer">
-            <label id = "question-8" className = "question-text">Question 8. What is the name of a person who makes sauces in a professional kitchen?  (2 pts)</label>
+            <label id = "question-8" className = "question-text">Question 8. What is the name of a person who bakes cookie?  (2 pts)</label>
             <select class="form-control" id="exampleFormControlSelect1" onChange = {(event) => props.onChangeSelectAnswers(event, 8)}>
               <option>Select Answer</option>
-              <option>Saucier</option>
-              <option>Sous chef</option>
+              <option>Baker</option>
+              <option>Chef</option>
               <option>Saucepertise</option>
             </select>
           </div>
           <div class="form-group  question-answer">
-            <label id = "question-9" className = "question-text">Question 9. Which of the following is an ingredient found in many Indian dishes?  (2 pts)</label>
+            <label id = "question-9" className = "question-text">Question 9. Which of the following is an ingredient not found in cookie?  (2 pts)</label>
             <select class="form-control" id="exampleFormControlSelect1" onChange = {(event) => props.onChangeSelectAnswers(event, 9)}>
               <option>Select Answer</option>
-              <option>Salt pork</option>
-              <option>Curry</option>
-              <option>Beef liver</option>
+              <option>Salt</option>
+              <option>Brown sugar</option>
+              <option>White sugar</option>
             </select>
           </div>
 
           <div class="form-group question-answer">
-            <label id = "question-10" className = "question-text">Question 10. Whipping egg whites will result in what?  (2 pts)</label>
+            <label id = "question-10" className = "question-text">Question 10. Can we store soft cookie in a container with lose lid (Yes/No)?  (2 pts)</label>
             <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="" onChange = {(event) => props.onChangeTextAnswers(event, 10)}/>
           </div>
 
