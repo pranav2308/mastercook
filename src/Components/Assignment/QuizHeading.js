1e2100f04da6232bdae7185e8ddd1d7c0d1fe231
pranav2308 <pranav08@terpmail.umd.edu>
2019-12-04 14:05:41 -0500

    Fixed Assignment color

--- src/Components/Assignment/QuizHeading.js
+++ src/Components/Assignment/QuizHeading.js
@@ -1,17 +1,52 @@
 import React from 'react';
+import firebase from "../../Firebase/firebase";
 
-const QuizHeading = (props) => {
-	return (
-		<div class = "headingContainer">
-	        <p> Course Name : Bake a cookie<br/>
-	            Quiz Name : Basics of Baking<br/>
-	            Assignment Info : Assignment No. 1 <br/>
-	            No. of Questions : 10 <br/>
-	            Attempts : 1 <br/>
-	            Maximum Marks : 20
-	        </p>
-	    </div>
-	);
+class QuizHeading extends React.Component{
+
+
+	constructor(props){
+		super(props);
+		this.state = {
+			courseName: '',
+			quizName: '',
+			totalQuestions: '',
+			attempts: '',
+			maximumMarks: ''
+		}
+	}
+
+	componentDidMount(){
+		const {assignmentID} = this.props;
+		firebase.firestore.collection('QuizContent').doc(assignmentID.toString()).onSnapshot(snapshot => {
+			const data = snapshot.data();
+			this.setState({
+				courseName: data.CourseName,
+				quizName: data.QuizName,
+				totalQuestions: data.TotalQuestions,
+				attempts: data.Attempts,
+				maximumMarks: data.MaximumMarks
+			})
+			
+		});	
+	}
+	
+
+	render(){
+		const {courseName, quizName, totalQuestions, attempts, maximumMarks } = this.state;
+		const {assignmentID} = this.props;
+		return (
+			<div class = "headingContainer">
+		        <p> Course Name : {courseName}<br/>
+		            Quiz Name : {quizName}<br/>
+		            Assignment Info : Assignment No. {assignmentID} <br/>
+		            No. of Questions : {totalQuestions} <br/>
+		            Attempts : {attempts} <br/>
+		            Maximum Marks : {maximumMarks}
+		        </p>
+		    </div>
+		);	
+	}
+	
 }
 
 
